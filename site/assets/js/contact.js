403d3e7a67943fdcbae07d9f0d303319957ff06d
P Nicolás Figliozzi <hola@nicolasfigliozzi.com.ar>
2018-01-30 13:39:40 +0100

    Contact form upgrade

--- site/assets/js/contact.js
+++ site/assets/js/contact.js
@@ -4,18 +4,16 @@ $(document).ready(function() {
   $contactForm.submit(function(evt) {
     var message = " name: " + $('#name').val() + '\r\n';
     message    += " email: " + $('#email').val() + '\r\n';
-    message    += " phone: " + $('#phone').val() + '\r\n';
     message    += " company: " + $('#company').val() + '\n';
-    message    += " Type of Product: " + $('#type-product').val() + '\n';
     message    += " Budget: " + $('#budget').val() + '\n';
     message    += " Description: " + $('#description').val() + '\n';
 
     $.ajax({
       type: 'POST',
-      url: 'https://rocketinsights-api.herokuapp.com/api/v1/contact',
+      url: 'false',
       data: { body: message },
       success: function () {
-        $('#content').html('<h3 class="text-center">Thank you, we will be in touch soon!</h3>');
+        $('form').html('<h3 class="text-center">Thank you, we will be in touch soon!</h3>');
         ga('send', 'event', {
           eventCategory: 'Form Submission',
           eventAction: 'click',
@@ -23,7 +21,7 @@ $(document).ready(function() {
         });
       },
       error: function (err) {
-        $('#content').html('<h3 class="text-center">Well this is embarrassing; we had an issue processing your request. Please contact us directly at aloha@rocketinsights.com.</h3>');
+        $('form').html('<h3 class="text-center">Well this is embarrassing; we had an issue processing your request. Please contact us directly at new@lateralview.co</h3>');
       }
     });
     evt.preventDefault();
