b59f0f7aa3ef29d8ac2b677e961312b6d3d599d7
P Nicolás Figliozzi <hola@nicolasfigliozzi.com.ar>
2018-01-30 12:46:29 +0100

    Smoth scroll to anchor

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -2,3 +2,11 @@
 $("#toggle").on("click",function(){
   $("#responsive-menu").toggleClass("open");
 })
+//smoth scroll to anchor
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
