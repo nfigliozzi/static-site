438b6a87364e1dfd9a283a823d1ab7a92e28d9f9
P Nicolás Figliozzi <hola@nicolasfigliozzi.com.ar>
2018-01-30 15:35:56 +0100

    send email

--- site/assets/js/contact.js
+++ site/assets/js/contact.js
@@ -6,25 +6,31 @@ $(document).ready(function() {
     message    += " email: " + $('#email').val() + '\r\n';
     message    += " company: " + $('#company').val() + '\n';
     message    += " Budget: " + $('#budget').val() + '\n';
-    message    += " Description: " + $('#description').val() + '\n';
+    message    += " Description: " + $('#description').val() + '\n',
+
+    from=$('#email').val(),
+    to= "new@lateralview.co";
 
     $.ajax({
       type: 'POST',
-      url: 'false',
-      data: { body: message },
-      success: function () {
-        $('form').html('<h3 class="text-center">Thank you, we will be in touch soon!</h3>');
-        ga('send', 'event', {
-          eventCategory: 'Form Submission',
-          eventAction: 'click',
-          eventLabel: 'Contact Us'
-        });
-      },
-      error: function (err) {
-        $('form').html('<h3 class="text-center">Well this is embarrassing; we had an issue processing your request. Please contact us directly at new@lateralview.co</h3>');
+      url: "https://lateralview.co/xhr/majorContact",
+      dataType: 'json',
+      data: { from:from, to:to, msg: message },
+      success: function () { emailSuccess(); },
+      error: function (response) {
+        if (response.status == 200) {
+          emailSuccess();
+        }
+        else {
+          $('form').html('<h3 class="text-center">Well this is embarrassing; we had an issue processing your request. Please contact us directly at '+to+'</h3>');
+        }
       }
     });
     evt.preventDefault();
     return false;
   })
 });
+
+function emailSuccess(){
+  $('form').html('<h3 class="text-center">Thank you, we will be in touch soon!</h3>');
+};
